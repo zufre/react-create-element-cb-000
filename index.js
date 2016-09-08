13c9065be35857722b2e9a025281482149875ed2
thomastuts <thomas@thomastuts.com>
2016-09-08 19:13:31 +0200

    Add completed code samples

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const app = "I don't do much.";
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { class: 'brown' }, 'Chocolate'),
+        React.createElement('li', { class: 'white' }, 'Vanilla'),
+        React.createElement('li', { class: 'yellow' }, 'Banana')
+      ]
+    ));
+
+ReactDOM.render(list, document.getElementById('main'));
